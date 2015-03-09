d8299d427321ac06d531ab714e6581acf5647bb2
Xiang <xiang@silvustechnologies.com>
2015-03-09 16:15:46 -0700

    try login into ucla..failed, session expired

--- cookie_login.py
+++ cookie_login.py
@@ -6,16 +6,24 @@ filename = "cookie.txt"
 cookie = cookielib.MozillaCookieJar(filename)
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
 postdata = urllib.urlencode({
-    'stuid':'asdfdsf',
-    'pwd': '123123'
+    'uc1e3f003b61290128d4763f80477232e':'',
+    'p97e25bce10ebc3b43262330d81f8fc47': ''
     })
 
-loginUrl = 'https://www.ursa.ucla.edu/index.php'
-result = opener.open(loginUrl, postdata)
+headers = {}
+user_agent = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
+#user agent
+headers['User-agent'] = user_agent
+
+loginUrl = 'https://auth-asm1.auth.ucla.edu/index.php'
+#loginUrl = 'https://auth.ucla.edu/index.php'
+request = urllib2.Request(loginUrl, postdata, headers)
+result = opener.open(request)
+print result.read()
 
 cookie.save(ignore_discard=True,ignore_expires=True)
 #some url to check grades, can only see after login
-gradeUrl = "safdsaf"
+gradeUrl = "https://be.my.ucla.edu/IWE/mygrades.aspx"
 
 result = opener.open(gradeUrl)
 print result.read()
