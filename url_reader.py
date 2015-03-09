d90c92db4f0effd0f16c261232077143b76d1d32
Xiang <xiang@silvustechnologies.com>
2015-03-09 15:33:12 -0700

    url header

--- url_reader.py
+++ url_reader.py
@@ -1,8 +1,29 @@
-#encoding:UTF-8
 import urllib, urllib2
 
-url = "http://www.google.com"
+values = {}
+values['username'] = 'bylixiang@gmail.com'
+values['password'] = '230432'
 
-request = urllib2.Request(url)
+#set header, if not, server may not respond
+headers = {}
+user_agent = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36'
+#user agent
+headers['User-agent'] = user_agent
+#referer
+headers['Referer'] = "http://www.zhihu.com/articles"
+'''
+other headers
+User-Agent : 有些服务器或 Proxy 会通过该值来判断是否是浏览器发出的请求
+Content-Type : 在使用 REST 接口时，服务器会检查该值，用来确定 HTTP Body 中的内容该怎样解析。
+application/xml ： 在 XML RPC，如 RESTful/SOAP 调用时使用
+application/json ： 在 JSON RPC 调用时使用
+application/x-www-form-urlencoded ： 浏览器提交 Web 表单时使用
+在使用服务器提供的 RESTful 或 SOAP 服务时， Content-Type 设置错误会导致服务器拒绝服务
+'''
+
+url = "http://www.zhihu.com"
+
+data = urllib.urlencode(values)
+request = urllib2.Request(url, data, headers)
 response = urllib2.urlopen(request)
 print response.read()
